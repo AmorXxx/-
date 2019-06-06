c1d42d49f8c0795ae74e6c6f3651a22f729cf6a6
Amor <274310994@qq.com>
2019-06-06 22:46:53 +0800

    修复数据库只能insert不能update的bug

--- MySQLComm.py
+++ MySQLComm.py
@@ -31,24 +31,23 @@ class MySQLCommand(object):
     # 插入数据，插入之前先查询是否存在，如果存在就不再插入
     def insertData(self, my_dict):
         for d in my_dict:
-            sqlexist = "SELECT id FROM send_grade  WHERE stu_no = '%s' and course_no = '%s' and score = '%s'" % (
-                d['stu_no'], d['course_no'], d['score'])
+            sqlexist = "SELECT id FROM send_grade  WHERE stu_no = '%s' and course_no = '%s'" % (
+                d['stu_no'], d['course_no'])
             res = self.cursor.execute(sqlexist)
 
             if res:  # res为查询到的数据条数如果大于0就代表数据已经存在
                 #数据存在执行更新操作
                 try:
                     updsql = """UPDATE send_grade SET score = '%s' WHERE stu_no = '%s' and course_no = '%s'""" % (d['score'], d['stu_no'], d['course_no'])
-                    try:
-                        re = self.cursor.execute(updsql)
-                        if re:
+                    re = self.cursor.execute(updsql)
+                    if re:
                             print('Update score successfully!')
-                        else:
+                    else:
                             print('Data has not changed')
-                    except pymysql.Error as e:
-                        # 发生错误时回滚
-                        self.conn.rollback()
-                        print("Update data failed，because %d: %s" % (e.args[0], e.args[1]))
+                except pymysql.Error as e:
+                    # 发生错误时回滚
+                    self.conn.rollback()
+                    print("Update data failed，because %d: %s" % (e.args[0], e.args[1]))
                 except pymysql.Error as e:
                     print("DBError，because %d: %s" % (e.args[0], e.args[1]))
 
@@ -59,17 +58,16 @@ class MySQLCommand(object):
                 try:
                     sql = """INSERT INTO send_grade VALUES (%d,'%s','%s','%s','%s','%s',%s,'%d')""" % (
                         d['id'], d['stu_no'], d['course_no'], d['score'], d['course_type'],  d['course_name'], d['count'], d['send'])
-                    try:
-                        result = self.cursor.execute(sql)
-                        insert_id = self.conn.insert_id()  # 插入成功后返回的id
-                        self.conn.commit()
-                        # 判断是否执行成功
-                        if result:
-                            print("Inserted successfully! ", insert_id)
-                    except pymysql.Error as e:
-                        # 发生错误时回滚
-                        self.conn.rollback()
-                        print("Interrupt data failed，because %d: %s" % (e.args[0], e.args[1]))
+                    result = self.cursor.execute(sql)
+                    insert_id = self.conn.insert_id()  # 插入成功后返回的id
+                    self.conn.commit()
+                    # 判断是否执行成功
+                    if result:
+                        print("Inserted successfully! ", insert_id)
+                except pymysql.Error as e:
+                    # 发生错误时回滚
+                    self.conn.rollback()
+                    print("Interrupt data failed，because %d: %s" % (e.args[0], e.args[1]))
                 except pymysql.Error as e:
                     print("DBError，because %d: %s" % (e.args[0], e.args[1]))
 
